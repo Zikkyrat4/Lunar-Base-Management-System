7d7a7e1762101f89224da1044e0b9eda43690b99
Даниил Прокопенко <zikkyrat@MacBook-Air-Daniil.local>
2025-05-14 00:51:44 +0300

    fix ip

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { store } from '../store';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://backend:8000/api/v1",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000/api/v1",
   timeout: 600000,
   headers: {
     'Content-Type': 'application/json',
